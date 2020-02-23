8a3a2e959aca8adff18c435c5065b0a819c8dba0
gridranger <bardos.david@gmail.com>
2020-02-23 22:00:34 +0100

    missing arguments error has been handled

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from sys import argv
 from bookbuilder import BookBuilder
 
-b = BookBuilder(argv[1], argv[2])
+
+try:
+    b = BookBuilder(argv[1], argv[2])
+except IndexError:
+    print("Please run it as 'python main.py input_folder_path output_file_path'.")
+    exit(1)
 b.convert()
